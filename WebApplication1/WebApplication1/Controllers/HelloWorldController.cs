85452de763704c1e233657086c1fe0a621b78668
Wiktor Kaczor <s27599@pjwstk.edu.pl>
2024-06-17 11:45:10 +0200

    project added

---
+++ WebApplication1/WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    // GET: /HelloWorld/Welcome/ 
+    public IActionResult Welcome(string name, int numTimes = 1)
+    {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
+    }
+}
